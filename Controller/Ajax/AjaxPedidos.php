4595ab0c06ae3a426ff1f3a5b2344ae0c022996b
Carlos Freitas <casfreitas@yahoo.com.br>
2021-12-27 09:07:53 -0300

    Configurado store procidure para os itens dos pedidos

--- Controller/Ajax/AjaxPedidos.php
+++ Controller/Ajax/AjaxPedidos.php
@@ -3,15 +3,17 @@ require_once('../../conexao/conexao_sqlsrv.php');
 
 $pedidoProd = $_POST['pedido'];
 
-$query = "SELECT *
-          FROM BomixBi.dbo.Fat_TB_PedidoVendaItem
-          WHERE Pedido_FK = '$pedidoProd'
-            ";
+// $query = "SELECT *
+//           FROM BomixBi.dbo.Fat_TB_PedidoVendaItem
+//           WHERE Pedido_FK = '$pedidoProd'
+//             ";
+
+$query = "Exec [BomixForce].[dbo].[Bomix_GetPedidoVendaItem] '$pedidoProd'";
 
 $execute = sqlsrv_query($conn, $query);
 
 while ($result = sqlsrv_fetch_array($execute, SQLSRV_FETCH_ASSOC)) {
-    $pedidoProd     = $result['Pedido_FK'];
+    //$pedidoProd     = $result['Pedido_FK'];
     $produtoProd    = $result['Produto'];
     $quantidadeProd = $result['Quantidade'];
     $personaliProd  = $result['Personalizacao'];
